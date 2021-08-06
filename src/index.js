4cfd8b5492a3fc32f276b294c5c729219b37386f
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-08-06 22:14:54 +0200

    install and deploy Redux.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// REDUX
+import {Provider} from 'react-redux';
+import store from './Redux/store';
+
+
 ReactDOM.render(
   <React.StrictMode>
+     <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
