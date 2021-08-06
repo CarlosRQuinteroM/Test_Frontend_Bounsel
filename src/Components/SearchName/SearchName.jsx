4413f0657adc3609a2adeb9b298ca7070fff9ddc
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-08-07 01:15:16 +0200

    Create conection with the DataBase and Add id Imput.

--- src/Components/SearchName/SearchName.jsx
+++ src/Components/SearchName/SearchName.jsx
@@ -1,18 +1,64 @@
-import React from 'react'
+import axios from 'axios';
+import React ,{ useState , useEffect}  from 'react'
 import { Button, Form } from "react-bootstrap";
 import { connect } from "react-redux";
 
 const SearchName = (props) => {
+
+    const [TimeUsers, setTimeUser] = useState([]);
+    console.log(TimeUsers);
+
+ useEffect(() => {
+    setTimeout(() => {
+        SearchDate()
+    }, 500);
+ }, [])
+
+   const SearchDate = async  () =>{
+        let id = props.credentials.data?.user.id;
+        let token = props.credentials.data?.token;
+        console.log(token);
+        try {
+            let res = await axios.get(`http://[::1]:3002/time-reports/findone/${id}`)
+
+            setTimeUser(res.data);
+            
+        } catch (error) {
+            console.log(error)
+        }
+
+     
+
+    }
+
+
+
+
+
+
     let name = props.credentials.data?.user.name;
     let lastName = props.credentials.data?.user.lastName;
     return (
-      
         <div className="FormCard">
           <div className="AlingForm">
             <Form>
                 <h1 id="Logintitle">{`Hola ${name} ${lastName} puedes buscar! `}</h1>
               <Form.Group className="mb-3" controlId="formBasicEmail">
-              <br></br><br></br>
+              <br/>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Add the User Id number</Form.Label>
+                <Form.Control
+                  type="number"
+                  name="ID"
+                  placeholder="Id User"
+
+                //   onChange={updateCredentials}
+                />
+                <Form.Text className="text-muted">
+                  
+                </Form.Text>
+              </Form.Group>
+
               <Form.Label>From the date</Form.Label>
                 <Form.Control
                   type="Date"
@@ -38,7 +84,7 @@ const SearchName = (props) => {
               <Button
                 className="BtnLogin1"
                 type="submit"
-                // onClick={() => SearchDate()}
+                onClick={() => SearchDate()}
               >
                 Search
               </Button>
