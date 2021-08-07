5f7ae29894cc54a5d5da529677cb824bb26d859f
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-08-07 13:57:23 +0200

    time-record start add to redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,13 +3,13 @@ import { save, load } from "redux-localstorage-simple";
 import reducer from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware(
-	save({ states: ['credentials'] }),
+	save({ states: ['credentials','TIMEReducer'] }),
     
 )(createStore);
 
 const store = createStoreWithMiddleware(
     reducer,
-    load({ states: ['credentials'] }),
+    load({ states: ['credentials','TIMEReducer'] }),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
         trace: true,
     })
--- src/Redux/type.js
+++ src/Redux/type.js
@@ -1,2 +1,4 @@
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
+export const STARTIME = "STARTIME";
+export const ENDTIME = "ENDTIME";
